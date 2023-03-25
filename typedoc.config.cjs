9f8d83f9e18f7de5c1ed5ade7f3f7c1ef58975d4
Ernest Irabor <irabeny89@gmail.com>
2023-03-25 02:39:30 +0100

    refactor: add typedoc config file and update package.json scripts

---
+++ typedoc.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  entryPoints: ['./src/index.ts'],
+  out: './docs',
+  includeVersion: true,
+  readme: './README.md',
+  hideGenerator: true
+}
